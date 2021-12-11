fb31b7cdd04de331734068104c76ae4f3a213c1c
Jhon Francis A. Duarte <94598599+jhonfrancisduarte@users.noreply.github.com>
2021-12-11 11:48:29 +0800

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,6 +1,14 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+import hoverEffect from 'hover-effect'
 
+var hoverDistort = new hoverEffect({
+    parent: document.querySelector('.pic-wrapper'),
+    intensity: 0.3,
+    image1: 'images/pic1.jpg',
+    image2: 'images/pic2.jpg',
+    displacementImage: 'images/4.png'
+});
 
 const bg = document.querySelector("section.bg");
 // initializing objects to the page
