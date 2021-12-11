a935caa9fdffbe9160b3be908c0d7d75259f8077
Jhon Francis A. Duarte <94598599+jhonfrancisduarte@users.noreply.github.com>
2021-12-11 17:53:06 +0800

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,17 +1,15 @@
 <?php
-//get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "jhonfrancisduarte12345@gmail.com";
 $subject = "Mail from my website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$headers = "From: noreply@yoursite.com" . "\r\n" . "CC: " . $email;
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
 //header("Location:thankyou.html");
-echo "Thank you for contacting me!";
+echo '<script>alert("Thank you for contacting me!")</script>';
 ?>
